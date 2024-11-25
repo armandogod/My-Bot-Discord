c2495ffbae89b97504ca06fabeaadb59ab7f0d25
Saharat Paksup <43146805+armandogod@users.noreply.github.com>
2024-11-25 20:12:38 +0700

    Update yo.js

--- yo.js
+++ yo.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const botPik = new Discord.Client();
 
-botPik.login('NDcwNDY1ODcyMzE4ODI0NDQ4.D02UmQ.61SMREPf1O6qdSaJ6Spr4kqgk0M')
+botPik.login('insert bot token')
 
 botPik.on("ready", () => {
   console.log('ArmandoGOD ready!')
@@ -14,9 +14,6 @@ botPik.on("message", (msg) => {
 	if (msg.content === "ปิ๊ก"){
 		msg.channel.send("กูไม่ใช่ปิ๊ก กูคือบอทที่แมวเดินผ่านคีย์บอร์ดเฉยๆ")
 	}
-	if (msg.content === "ทำอะไรอยู่"){
-		msg.channel.send("ทำอะไรก็ได้กูไม่ได้ขอตังใคร")
-	}
 	if (msg.content === "แง๊นๆ"){
 		msg.channel.send("แง๊นพ่อมึงดิ")
 	}
@@ -28,16 +25,7 @@ botPik.on("message", (msg) => {
 	}
 	if (msg.content === "ขอโทษครับท่าน"){
 		msg.channel.send("เดี๋ยวทุ่มด้วยโพเดียมเลย")
-	}
-	if (msg.content === "อรอุมาคือใครวะ"){
-		msg.channel.send("กูก้ไม่รู้ว่าใคร แม่ไออายมันมั้ง")
-	}
-	if (msg.content === "แป้งคือใครวะ"){
-		msg.channel.send("กูก้ไม่รู้ว่าใคร คนที่ไอนิวแอบชอบมั้ง")
-	}	
-	if (msg.content === "ออมคือใครวะ"){
-		msg.channel.send("กูก้ไม่รู้ว่าใคร แฟนเก่าไอนิวมันมั้ง")
-	}	
+	}		
 	if (msg.content === "ก็ตกกะปิหนิ"){
 		msg.channel.send("ตกกะปิก็เหี้ยแล้ว")
 	}	
@@ -49,6 +37,6 @@ botPik.on("message", (msg) => {
 	}
 	if (!msg.content.startsWith("/")) return;
   if (msg.content.slice(1).split(" ")[0] === "commands") {
-      msg.channel.send("คำสั่ง:อิอิ ปิีก ทำอะไรอยู่ แง๊นๆ เป็นเพื่อนจะดีกว่า ก้มาดิค้าบบ ขอโทษครับท่าน อรอุมาคือใครวะ แป้งคือใครวะ ออมคือใครวะ ก็ตกกะปิหนิ");
+      msg.channel.send("คำสั่ง:อิอิ ปิีก ทำอะไรอยู่ แง๊นๆ เป็นเพื่อนจะดีกว่า ก็มาดิค้าบบ ขอโทษครับท่าน ก็ตกกะปิหนิ");
 	}
 })
